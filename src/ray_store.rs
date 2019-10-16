d0aeefe5d67248ec705bb9f29e0fdddba3c4e681
alekspickle <aleks.work2222@gmail.com>
2019-10-16 16:32:52 +0300

    scene: impl default

--- src/ray_store.rs
+++ src/ray_store.rs
@@ -58,3 +58,26 @@ impl Zero for Point {
         }
     }
 }
+
+impl Default for Scene {
+    fn default() -> Scene {
+        Scene {
+            width: 800,
+            height: 600,
+            fov: 90.0,
+            sphere: Sphere {
+                center: Point {
+                    x: 0.0,
+                    y: 0.0,
+                    z: -5.0,
+                },
+                radius: 1.0,
+                color: Color {
+                    red: 0.4,
+                    green: 1.0,
+                    blue: 0.4,
+                },
+            },
+        }
+    }
+}
