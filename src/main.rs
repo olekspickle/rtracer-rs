8b727fd76083b5d8e555cef7c94f966f05902ce7
alekspickle <aleks.work2222@gmail.com>
2019-10-28 17:43:21 +0200

    fractal: add image saving

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,46 @@
 use std::path::Path;
-
-use image::{DynamicImage, GenericImageView};
+use image::{DynamicImage, GenericImageView, Rgba};
 
 mod entities;
+mod fractal;
 mod my_macro;
 mod rendering;
 
 use entities::{Color, Point, Scene, Sphere};
+use fractal::{Fractal};
+use rendering::{Intersectable, Ray};
 
 fn main() {
     let scene = Scene::default();
     let img: DynamicImage = scene.render();
-    let _result = save_image(img, &Path::new("output/test.png"));
+    let f = Fractal::default();
+
+    save_image(img, &Path::new("output/test_scene.png"));
+    f.save(&Path::new("output/fractal.png"));
 }
 
+// pub fn render(scene: &Scene) -> DynamicImage {
+//     let mut image = DynamicImage::new_rgb8(scene.width, scene.height);
+//     let black = Rgba::from_channels(0, 0, 0, 0);
+//     for x in 0..scene.width {
+//         for y in 0..scene.height {
+//             let ray = Ray::create_prime(x, y, scene);
+
+//             if scene.sphere.intersect(&ray) {
+//                 image.put_pixel(x, y, to_rgba(&scene.sphere.color))
+//             } else {
+//                 image.put_pixel(x, y, black);
+//             }
+//         }
+//     }
+//     image
+// }
+
 pub fn save_image(img: DynamicImage, p: &Path) {
+    println!("saving image to {:?} ", p);
+
     match img.save(p) {
-        Ok(ok) => println!("saved successfully {:?}", ok),
+        Ok(_) => println!("saved successfully"),
         Err(err) => println!("failed to save {:?}", err),
     }
 }
