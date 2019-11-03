a97c9116a61e0839bda242c14bf585bbd3b7aceb
alekspickle <aleks.work2222@gmail.com>
2019-11-03 18:15:01 +0200

    refactor more logical arch

--- src/entities.rs
+++ src/entities.rs
@@ -1,6 +1,3 @@
-use crate::rendering::{Intersectable, Ray};
-
-use image::{DynamicImage, GenericImage, Pixel, Rgba};
 use num_traits::identities::Zero;
 use serde_derive::{Deserialize, Serialize};
 use std::ops::{Add, Sub};
@@ -146,27 +143,5 @@ impl Default for Scene {
     }
 }
 
-impl Scene {
-    pub fn render(&self) -> DynamicImage {
-        let mut image = DynamicImage::new_rgb8(self.width, self.height);
-        let background = Rgba::from_channels(0, 0, 0, 0);
-        let sphere_color = Rgba::from_channels(
-            self.sphere.color.red as u8,
-            self.sphere.color.green as u8,
-            self.sphere.color.blue as u8,
-            0,
-        );
-        for x in 0..self.width {
-            for y in 0..self.height {
-                let ray = Ray::create_prime(x, y, self);
 
-                if self.sphere.intersect(&ray) {
-                    image.put_pixel(x, y, sphere_color)
-                } else {
-                    image.put_pixel(x, y, background);
-                }
-            }
-        }
-        image
-    }
-}
+
