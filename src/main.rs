dd381299d4ed08240053a658e314ea6924ca5684
Aleks Gnatovskyi <aleks.work2222@gmail.com>
2020-02-22 15:01:04 +0200

    update: texturing

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,16 @@
 //! [![v](https://img.shields.io/badge/v-0.0.4-blueviolet)]()
 //! # ray tracer in rust
 //! 
-//! Here is attempt to write ray-tracer in rust-lang
+//! Thanks to amazing [criterion author](https://github.com/bheisler) for his project! 
+//! This stuff is hard for me personally without any background in rendering and 
+//! basic understanding of linear transformations.
+//! Also some parts are different two years later, so I am not able to blindly re-type all of the code anyway.
+//! And this is great experience! <3
 //! 
 //! 1. Simple sphere 
 //! 2. Spheres on a plane 
 //! 3. Basic shadows
-//! 
+//! 4. Texturing: using vector products to calculate texture
 //! 
 //! ### useful resourses
 //! [Scrathpixel](https://www.scratchapixel.com/index.php?redirect) is fantastic library! 
