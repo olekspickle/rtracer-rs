f6d26947210f9c8c500484ab529a4152b34f6f25
Aleks Pickle <aleks.work2222@gmail.com>
2022-08-03 20:45:02 +0300

    fix vuln crates, delete heavy image

--- build.rs
+++ build.rs
@@ -1,9 +1,5 @@
 use std::fs::create_dir;
 
 fn main() {
-    //create 'output' directory
-    match create_dir("output") {
-        Ok(result) => println!("created output folder {:?}", result),
-        Err(result) => println!("failed to create output folder {}", result),
-    }
+    let _ = create_dir("output");
 }
