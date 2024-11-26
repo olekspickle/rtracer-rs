258ed2181e33e081d2b0399a695add757b43df79
olekspickle <22867443+olekspickle@users.noreply.github.com>
2024-11-26 09:57:27 +0100

    fix vec types

--- src/rendering.rs
+++ src/rendering.rs
@@ -4,7 +4,7 @@ use num_traits::identities::Zero;
 
 pub struct Ray {
     pub origin: Point,
-    pub direction: Vec3<f32>,
+    pub direction: Vec3<f64>,
 }
 
 impl Ray {
@@ -16,17 +16,16 @@ impl Ray {
     }
 }
 
-// pub fn create_prime(x: u32, y: u32, scene: &Scene) -> Ray {
-//     let sensor_x = ((x as f64 + 0.5) / scene.width as f64) * 2.0 - 1.0;
-//     let sensor_y = 1.0 - ((y as f64 + 0.5) / scene.height as f64) * 2.0;
+pub fn create_prime(x: u32, y: u32, scene: &Scene) -> Ray {
+    let sensor_x = ((x as f64 + 0.5) / scene.width as f64) * 2.0 - 1.0;
+    let sensor_y = 1.0 - ((y as f64 + 0.5) / scene.height as f64) * 2.0;
 
-//     Ray {
-//         origin: Point::zero(),
-//         direction: Vec3 {
-//                 x: sensor_x,
-//                 y: sensor_y,
-//                 z: -1.0,
-//             }
-//             .normalize(),
-//     }
-// }
+    Ray {
+        origin: Point::zero(),
+        direction: Vec3 {
+                x: sensor_x,
+                y: sensor_y,
+                z: -1.0,
+            },
+    }
+}
