8345d2c969581f49b3ca345b85736321ce25242f
olekspickle <22867443+olekspickle@users.noreply.github.com>
2024-11-26 09:57:27 +0100

    add test case

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,42 @@
 use serde_derive;
 use vek;
+use image::{DynamicImage};
 
 mod ray_store;
-
+use ray_store::{Scene, Sphere, Color, Point};
 
 fn main() {
     println!("Hello, world!");
+}
+
+pub fn render(scene: &Scene) -> DynamicImage {
+    DynamicImage::new_rgb8(scene.width, scene.height)
+}
+
+#[test]
+fn test_can_render_scene() {
+    use image::DynamicImage;
+
+    let scene = Scene {
+        width: 800,
+        height: 600,
+        fov: 90.0,
+        sphere: Sphere {
+            center: Point {
+                x: 0.0,
+                y: 0.0,
+                z: -5.0,
+            },
+            radius: 1.0,
+            color: Color {
+                red: 0.4,
+                green: 1.0,
+                blue: 0.4,
+            },
+        },
+    };
+
+    let img: DynamicImage = render(&scene);
+    assert_eq!(scene.width, img.width());
+    assert_eq!(scene.height, img.height());
 }
