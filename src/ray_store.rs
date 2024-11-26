09d8e7bd71cab6dedfa282b5fb580cf12c4218f9
olekspickle <22867443+olekspickle@users.noreply.github.com>
2024-11-26 09:57:27 +0100

    start custom approach

--- src/ray_store.rs
+++ src/ray_store.rs
@@ -1,9 +1,36 @@
 use serde_derive::Deserialize;
 use vek::Vec3;
 
-#[derive(Debug, Deserialize)]
-pub struct Ray {
-    // A: Vec3<u32>,
-    // B: Vec3<u32>,
-    time: f32,
+// #[derive(Debug, Deserialize)]
+// pub struct Ray {
+//     A: Vec3<f32, f32, f32>,
+//     B: Vec3<f32, f32, f32>,
+//     time: f32,
+// }
+
+
+
+pub struct Point {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
 }
+
+pub struct Color {
+    pub red: f32,
+    pub green: f32,
+    pub blue: f32,
+}
+
+pub struct Sphere {
+    pub center: Point,
+    pub radius: f64,
+    pub color: Color,
+}
+
+pub struct Scene {
+    pub width: u32,
+    pub height: u32,
+    pub fov: f64,
+    pub sphere: Sphere,
+}   
