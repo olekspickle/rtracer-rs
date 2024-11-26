26c3a04bb6baa6b0b234e0ddd464072d7d9cb665
olekspickle <22867443+olekspickle@users.noreply.github.com>
2024-11-26 09:57:27 +0100

    add prime camera ray

---
+++ src/rendering.rs
@@ -0,0 +1,30 @@
+
+
+pub struct Ray {
+    pub origin: Point,
+    pub direction: Vector3,
+}
+
+impl Ray {
+    pub fn create_prime(x: u32, y: u32, scene: &Scene) -> Ray {
+        Ray {
+            origin: Point::zero(),
+            direction: Vector3::zero(),
+        }
+    }
+}
+
+pub fn create_prime(x: u32, y: u32, scene: &Scene) -> Ray {
+    let sensor_x = ((x as f64 + 0.5) / scene.width as f64) * 2.0 - 1.0;
+    let sensor_y = 1.0 - ((y as f64 + 0.5) / scene.height as f64) * 2.0;
+
+    Ray {
+        origin: Point::zero(),
+        direction: Vector3 {
+                x: sensor_x,
+                y: sensor_y,
+                z: -1.0,
+            }
+            .normalize(),
+    }
+}
