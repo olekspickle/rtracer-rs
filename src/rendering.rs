76a7c25a03d36bb2ad3c2aba7333ea4809c2b91a
olekspickle <22867443+olekspickle@users.noreply.github.com>
2024-11-26 09:57:27 +0100

    rendering: import right vec3

--- src/rendering.rs
+++ src/rendering.rs
@@ -1,30 +1,32 @@
-
+use crate::ray_store::{Point, Scene};
+use vek::{Vec3};
+use num_traits::identities::Zero;
 
 pub struct Ray {
     pub origin: Point,
-    pub direction: Vector3,
+    pub direction: Vec3,
 }
 
 impl Ray {
     pub fn create_prime(x: u32, y: u32, scene: &Scene) -> Ray {
         Ray {
             origin: Point::zero(),
-            direction: Vector3::zero(),
+            direction: Vec3::zero(),
         }
     }
 }
 
-pub fn create_prime(x: u32, y: u32, scene: &Scene) -> Ray {
-    let sensor_x = ((x as f64 + 0.5) / scene.width as f64) * 2.0 - 1.0;
-    let sensor_y = 1.0 - ((y as f64 + 0.5) / scene.height as f64) * 2.0;
+// pub fn create_prime(x: u32, y: u32, scene: &Scene) -> Ray {
+//     let sensor_x = ((x as f64 + 0.5) / scene.width as f64) * 2.0 - 1.0;
+//     let sensor_y = 1.0 - ((y as f64 + 0.5) / scene.height as f64) * 2.0;
 
-    Ray {
-        origin: Point::zero(),
-        direction: Vector3 {
-                x: sensor_x,
-                y: sensor_y,
-                z: -1.0,
-            }
-            .normalize(),
-    }
-}
+//     Ray {
+//         origin: Point::zero(),
+//         direction: Vec3 {
+//                 x: sensor_x,
+//                 y: sensor_y,
+//                 z: -1.0,
+//             }
+//             .normalize(),
+//     }
+// }
