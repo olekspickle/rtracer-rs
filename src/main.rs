ea580ee69bdc61a37e7b7204cb49e745ff2b7df8
olekspickle <22867443+olekspickle@users.noreply.github.com>
2024-11-26 09:57:28 +0100

    remove test stuff

--- src/main.rs
+++ src/main.rs
@@ -31,28 +31,3 @@ pub fn save_image(img: DynamicImage, p: &Path) {
         Err(err) => println!("failed to save {:?}", err),
     }
 }
-
-pub fn test_save_image(img: DynamicImage, p: &Path) -> bool {
-    match img.save(p) {
-        Ok(_) => true,
-        Err(_) => false,
-    }
-}
-
-mod test {
-    #![allow(unused)]
-    use super::*;
-    use image::{GenericImageView};
-
-
-    #[test]
-    fn test_can_render_simple_sphere() {
-        let scene = Scene::default();
-        let img: DynamicImage = scene.render_simple_sphere();
-        assert_eq!(scene.width, img.width());
-        assert_eq!(scene.height, img.height());
-
-        let result = test_save_image(img, &Path::new("output/test.png"));
-        assert_eq!(result, true);
-    }
-}
