264338a419ba3fb0d7dc0fdae3ebb60349a781ff
olekspickle <22867443+olekspickle@users.noreply.github.com>
2025-02-21 22:46:34 +0100

    main: do smth useful in main

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,60 @@
 use std::{boxed::Box, path::Path, sync::Arc};
 
+use image::{DynamicImage, ImageDecoder};
 use serde_derive;
 use vek;
-use image::{DynamicImage, ImageDecoder};
 
+mod my_macro;
 mod ray_store;
 mod rendering;
-mod my_macro;
 
-use ray_store::{Scene, Sphere, Color, Point};
+use ray_store::{Color, Point, Scene, Sphere};
 
 fn main() {
-    println!("Hello, world!");
+    let scene = Scene::default();
+
+    let img: DynamicImage = render(&scene);
+    let result = match img.save(&Path::new("output/test.png")) {
+        Ok(ok) => {
+            println!("saved successfully {:?}", ok);
+            true
+        }
+        Err(err) => {
+            println!("failed to save {:?}", err);
+            false
+        }
+    };
 }
 
 pub fn render(scene: &Scene) -> DynamicImage {
     DynamicImage::new_rgb8(scene.width, scene.height)
 }
 
-
-
-mod test{
+mod test {
     use super::*;
 
     #[test]
     fn test_can_render_scene() {
         use image::DynamicImage;
 
-        let scene = Scene {
-            width: 800,
-            height: 600,
-            fov: 90.0,
-            sphere: Sphere {
-                center: Point {
-                    x: 0.0,
-                    y: 0.0,
-                    z: -5.0,
-                },
-                radius: 1.0,
-                color: Color {
-                    red: 0.4,
-                    green: 1.0,
-                    blue: 0.4,
-                },
-            },
-        };
+        let scene = Scene::default();
 
         let img: DynamicImage = render(&scene);
-        match img.save(&Path::new("/output/test.png")) {
-            Ok(ok) => println!("saved successfully {:?}", ok),
-            Err(err) => println!("failed to save {:?}", err),
-        }
-        
-        // TODO: width & height functions are public in ImageBuffer struct 
-        // I have no idea why it is broken 
+        let result = match img.save(&Path::new("output/test.png")) {
+            Ok(ok) => {
+                println!("saved successfully {:?}", ok);
+                true
+            }
+            Err(err) => {
+                println!("failed to save {:?}", err);
+                false
+            }
+        };
+        assert_eq!(result, true);
+
+        // TODO: width & height functions are public in ImageBuffer struct
+        // I have no idea why it is broken
         // assert_eq!(scene.width, img.width());
         // assert_eq!(scene.height, img.height());
     }
 }
-
