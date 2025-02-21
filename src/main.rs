576b4db78e5aadb8ac094c65672e948bcb13b34a
olekspickle <22867443+olekspickle@users.noreply.github.com>
2025-02-21 22:46:35 +0100

    add image to readme

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,60 @@
-
-//! [![v](https://img.shields.io/badge/v-0.0.4-blueviolet)]()
+//! [![v](https://img.shields.io/badge/v-0.0.5-blueviolet)]()
 //! # ray tracer in rust
-//! 
-//! Thanks to amazing [criterion author](https://github.com/bheisler) for his project! 
-//! This stuff is hard for me personally without any background in rendering and 
+//! [![raytracing example](/pics/example)]
+//!
+//! Thanks to amazing [criterion author](https://github.com/bheisler) for his raytraycing project!
+//!
+//! It turned out to be hell of a mutch bigger deal I initially thought it would. 
+//! Consider this a version update. 
+//!
+//! This stuff is hard for me personally without any background in rendering and
 //! basic understanding of linear transformations.
 //! Also some parts are different two years later, so I am not able to blindly re-type all of the code anyway.
 //! And this is great experience! <3
-//! 
-//! 1. Simple sphere 
-//! 2. Spheres on a plane 
+//!
+//! 1. Simple sphere
+//! 2. Spheres on a plane
 //! 3. Basic shadows
 //! 4. Texturing: using vector products to calculate texture
-//! 
+//! 5. Reflections: basic implementation with recursive restriction.
+//!
 //! ### useful resourses
-//! [Scrathpixel](https://www.scratchapixel.com/index.php?redirect) is fantastic library! 
+//! [Scrathpixel](https://www.scratchapixel.com/index.php?redirect) is fantastic library!
 //! They have great materials and specific images and docs! You should definately check them out.
-//! 
+//!
 use image::DynamicImage;
-use std::{path::Path, fs::File};
+use std::{fs::File, path::Path, time::Instant};
 
 mod entities;
 mod fractal;
 mod point;
 mod rendering;
+mod scene;
 mod utils;
 mod vector;
-mod scene;
 
 pub use entities::*;
 pub use fractal::*;
 pub use point::*;
 pub use rendering::*;
+use scene::Scene;
 pub use utils::*;
 pub use vector::*;
-use scene::{Scene};
 
 pub fn main() {
     let scene_path = Path::new("scenes/test.json");
     let scene_file = File::open(scene_path).expect("File not found");
     let scene: Scene = serde_json::from_reader(scene_file).unwrap();
 
+    let start = Instant::now();
+    println!("Start ray tracing image render...");
     let img: DynamicImage = scene.render();
-    let f = Fractal::default();
-
     save_image(img, &Path::new("output/test_scene.png"));
+    println!("Elapsed: {:?}", start.elapsed());
+
+    let start = Instant::now();
+    println!("Start fractal image render...");
+    let f = Fractal::default();
     f.save(&Path::new("output/fractal.png"));
+    println!("Elapsed: {:?}", start.elapsed());
 }
-
-
