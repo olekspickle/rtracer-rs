e4a02d1c06a39820520406e26d17d733f622e3dd
olekspickle <22867443+olekspickle@users.noreply.github.com>
2025-02-21 22:46:35 +0100

    add description for cargo readme

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+
+//! [![v](https://img.shields.io/badge/v-0.0.4-blueviolet)]()
+//! # ray tracer in rust
+//! 
+//! Here is attempt to write ray-tracer in rust-lang
+//! 
+//! 1. Simple sphere 
+//! 2. Spheres on a plane 
+//! 3. Basic shadows
+//! 
+//! 
+//! ### useful resourses
+//! [Scrathpixel](https://www.scratchapixel.com/index.php?redirect) is fantastic library! 
+//! They have great materials and specific images and docs! You should definately check them out.
+//! 
 use image::DynamicImage;
 use std::{path::Path, fs::File};
 
