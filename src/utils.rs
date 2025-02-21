d67dcabf8e601efe08cc1fc73aaf45c435e8b6f4
olekspickle <22867443+olekspickle@users.noreply.github.com>
2025-02-21 22:46:35 +0100

    make texture loading right

--- src/utils.rs
+++ src/utils.rs
@@ -1,30 +1,43 @@
-use termion::{color, style};
-use image::DynamicImage;
+use image::{DynamicImage, ImageFormat};
+use std::fs::{File, OpenOptions};
 use std::path::Path;
+use termion::{color, style};
 
 #[macro_export]
 #[cfg(feature = "std")]
 macro_rules! assert_ok {
-	( $x:expr ) => {
-		assert_eq!($x, Ok(()));
-	};
-	( $x:expr, $y:expr ) => {
-		assert_eq!($x, Ok($y));
-	}
+    ( $x:expr ) => {
+        assert_eq!($x, Ok(()));
+    };
+    ( $x:expr, $y:expr ) => {
+        assert_eq!($x, Ok($y));
+    };
 }
 
 pub fn print_green(s: &str) {
-	println!("{}{}{}", color::Fg(color::Green), s, color::Fg(color::Reset))
+    println!(
+        "{}{}{}",
+        color::Fg(color::Green),
+        s,
+        color::Fg(color::Reset)
+    )
 }
 
 pub fn print_italic(s: &str) {
-	println!("{}{}{}", style::Italic, s, style::Reset);
+    println!("{}{}{}", style::Italic, s, style::Reset);
 }
 
 pub fn save_image(img: DynamicImage, p: &Path) {
     print_italic(&format!("saving as {:?}...", p));
 
-    match img.save(p) {
+    let mut _image_file: File = OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .create(true)
+        .open(p)
+        .unwrap();
+
+    match img.save_with_format(p, ImageFormat::PNG) {
         Ok(_) => print_green("success!"),
         Err(err) => println!("failed to save {:?}", err),
     }
--- src/vector.rs
+++ src/vector.rs
@@ -23,7 +23,7 @@ impl <'de>Vector3 {
     }
 
     pub fn norm(&self) -> f64 {
-        (self.x * self.x + self.y * self.y + self.z * self.z)
+        self.x * self.x + self.y * self.y + self.z * self.z
     }
 
     pub fn normalize(&self) -> Vector3 {
